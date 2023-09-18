0ce12a456b36e863e7e57b2f4ba7e1c3bce38083
Chanyoung Lee <chanyounglee0476@gamil.com>
2023-09-17 21:27:23 -0400

    added description in stcok purchase

---
+++ stockPurchas.js
@@ -0,0 +1,23 @@
+"use strict";
+/*
+Author: Chanyoung Lee
+Date: 9/17/2023
+Description: A practice for Tech-check
+
+A. Assign the value 25.625 to the variable cost_per_share.
+B. Assign the value 400 to the variable number_of_shares.
+C. Assign the product of cost_per_share and number_of_shares to the variable markdown.
+D. Display the value of the variable markdown in the console application.
+*/
+
+// DO NOT EDIT: The main function to house our program code 
+function main()
+{
+    // enter your code here
+}
+
+// DO NOT EDIT: Trigger our main function to launch the program
+if (require.main === module)
+{
+    main();
+}
